8c40226ec973c06c711041f046557b0c562f6c05
Alex Haban <Alex Haban@DESKTOP-QAPS4P4.lan>
2022-12-06 14:50:20 -0500

    Created Repo

---
+++ FinalProject/MainPackage/Main.py
@@ -0,0 +1,11 @@
+'''
+Name: Eyl
+Members: Alex Haban, Matthew Hall, Colin Bui, Roman Groenewold
+email: habanaj@mail.uc.edu
+Assignment: Final Project
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: 
+Citations:
+Anything else that's relevant:
+'''
