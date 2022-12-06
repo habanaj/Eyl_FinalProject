e507f3443bfb3f9c755680f4e5e9dcb25ab35e7c
Alex Haban <Alex Haban@DESKTOP-QAPS4P4.lan>
2022-12-06 16:04:50 -0500

    Looked up word indexes and it makes no sense

--- FinalProject/MainPackage/Main.py
+++ FinalProject/MainPackage/Main.py
@@ -9,3 +9,24 @@ Brief Description:
 Citations:
 Anything else that's relevant:
 '''
+
+Eyl = ["7479",
+       "28893",
+       "8017",
+       "42048",
+       "20572",
+       "42060",
+       "27510",
+       "6635",
+       "28893",
+       "8017"]
+Location = ["builders",
+            "omnivore",
+            "camps",
+            "thanksgivings",
+            "harvests",
+            "thaws",
+            "mossy",
+            "booklets",
+            "omnivore",
+            "camps"]
