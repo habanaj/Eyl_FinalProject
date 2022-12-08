468aa258976520afb3edd3e5ec798d140ef0a3dd
Alex Haban <Alex Haban@DESKTOP-QAPS4P4.lan>
2022-12-07 19:06:56 -0500

    Added Colin's email and copied comments from Main to image_functions

--- FinalProject/MainPackage/image_functions.py
+++ FinalProject/MainPackage/image_functions.py
@@ -1,7 +1,13 @@
 '''
-Created on Dec 6, 2022
-
-@author: Matthew Hall
+Name: Eyl
+Members: Alex Haban, Matthew Hall, Colin Bui, Roman Groenewold
+email: habanaj@mail.uc.edu, hall4mj@mail.uc.edu, groenern@mail.uc.edu, buici@mail.uc.edu
+Assignment: Final Project
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: 
+Citations:
+Anything else that's relevant:
 '''
 from PIL import Image, ImageFilter, ImageDraw, ImageFont
 import os, sys
